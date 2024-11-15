cb2570b4d2770346c7fa3d131ad20ffdaae41a9f
coatlicue89 <gonzalo@revert.finance>
2024-11-14 23:36:42 -0300

    Encode and decode function

---
+++ src/main/abi_clj/decode.clj
@@ -0,0 +1,95 @@
+(ns abi-clj.decode
+  (:require
+   [abi-clj.utils.hex :as utils.hex]
+   [abi-clj.utils.abi :as utils.abi]
+   [clojure.string :as str])
+  (:import
+   [java.math BigInteger]
+   [java.lang Integer]))
+
+(defn- decode-dispatch [{type :type}]
+  (cond
+    (re-matches #"^((uint|int|bool|address|bytes)(\d+)?)((\[\d+\])+)$" type) :vector
+    (= type "tuple")                   :tuple
+    (= type "bool")                    :bool
+    (= type "address")                 :address
+    (= type "string")                  :string
+    (re-matches #"^uint.*" type)       :unumber
+    (re-matches #"^int.*" type)        :number
+    (re-matches #"^bytes(\d+)$" type)  :bytes
+    (re-matches #"^bytes$" type)       :dbytes))
+
+(defmulti param #'decode-dispatch)
+
+(defmethod param :tuple
+  [{data :data
+    components :components}]
+  (let [value (str/replace data #"^0x" "")
+        with-names? (every? :name components)
+        res (loop [cursor 0 component (first components) components (rest components) res []]
+              (if component
+                (let [size (utils.abi/item->size component)
+                      decoded (param (merge component {:data (utils.hex/concat [(subs value cursor (+ cursor size))])}))
+                      append (if with-names?
+                               [(keyword (:name component)) decoded]
+                               decoded)]
+                  (recur (+ cursor size) (first components) (rest components) (conj res append)))
+                res))]
+
+    (if with-names?
+      (into {} res)
+      (vec res))))
+
+(defmethod param :vector
+  [{data :data
+    type :type}]
+  (let [match (re-matches #"^((uint|int|bool|address|bytes)(\d+)?)((\[\d+\])+)$" type)
+        type (second match)
+        dimension (nth match 4)
+        rem-dim (re-matches #"((\[\d+\])+)(\[\d+\])$" dimension)
+        conc-dim (when rem-dim (second rem-dim))
+        len (- (count data) 2)
+        split (->> (nth match 5)
+                   (re-matches #"^\[(\d+)\]")
+                   second
+                   Integer/parseInt
+                   (/ len))]
+    (->> (partition-all split (str/replace data #"^0x" ""))
+         (map #(param {:type (str type conc-dim) :data (utils.hex/concat [(str/join %)])}))
+         vec)))
+
+(defmethod param :unumber
+  [{data :data}]
+  (BigInteger. (str/replace data #"^0x" "") 16))
+
+(defmethod param :number
+  [{data :data}]
+  (let [val (BigInteger. (str/replace data #"^0x" "") 16)
+        max (.subtract (.shiftLeft (BigInteger. (str "1")) (- (* 32 8) 1)) (BigInteger. (str 1)))]
+    (if (< val max) val
+        (.subtract val (.shiftLeft (BigInteger. (str "1")) (* 32 8))))))
+
+(defmethod param :address
+  [{data :data}]
+  (utils.hex/concat [(-> data
+                         (str/replace #"^0x" "")
+                         (subs (- 64 40) 64))]))
+
+(defmethod param :bool
+  [{data :data}]
+  (< 0 (BigInteger. (str/replace data #"^0x" ""))))
+
+(defmethod param :bytes
+  [{data :data type :type}]
+  (let [size (->> (re-matches #"^bytes(\d+)$" type)
+                  second
+                  Integer/parseInt)]
+    (subs data 0 (+ 2 (* size 2)))))
+
+(defn event
+  [{abi-item :abi-item data :data}]
+  (param {:type "tuple" :components (:inputs abi-item) :data data}))
+
+(defn function-result
+  [{abi-item :abi-item data :data}]
+  (param {:type "tuple" :components (:outputs abi-item) :data data}))
---
+++ src/main/abi_clj/encode.clj
@@ -0,0 +1,96 @@
+(ns abi-clj.encode
+  (:require
+   [abi-clj.utils.abi :as utils.abi]
+   [abi-clj.utils.hex :as utils.hex]
+   [clojure.string :as str])
+  (:import
+   [java.math BigInteger]
+   [java.nio.charset StandardCharsets]
+   [org.bouncycastle.util.encoders Hex]))
+
+(defn- encode-dispatch [{type :type}]
+  (cond
+    (re-matches #"^((uint|int|bool|address|bytes)(\d+)?)((\[\d+\])+)$" type) :vector
+    (= type "tuple")                   :tuple
+    (= type "bool")                    :bool
+    (= type "address")                 :address
+    (= type "string")                  :string
+    (re-matches #"^uint.*" type)       :unumber
+    (re-matches #"^int.*" type)        :number
+    (re-matches #"^bytes(\d+)$" type)  :bytes
+    (re-matches #"^bytes$" type)       :dbytes))
+
+(defmulti param #'encode-dispatch)
+
+(defmethod param :tuple
+  [{value :value
+    components :components}]
+  (if (map? value)
+    (->> components
+         (map (fn [type] (param (merge type {:value (or (get value (keyword (:name type)))
+                                                        (get value (:name type)))}))))
+         utils.hex/concat)
+    (->> (zipmap components value)
+         (map (fn [[type value]] (param (merge type {:value value}))))
+         utils.hex/concat)))
+
+(defmethod param :vector
+  [{value :value
+    type  :type}]
+  (let [match (re-matches #"^((uint|int|bool|address|bytes)(\d+)?)((\[\d+\])+)$" type)
+        type (second match)
+        dimension (nth match 4)
+        rem-dim (re-matches #"((\[\d+\])+)(\[\d+\])$" dimension)
+        conc-dim (when rem-dim (second rem-dim))]
+    (->> value
+         (map #(param {:type (str type conc-dim) :value %}))
+         utils.hex/concat)))
+
+(defmethod param :bool
+  [{value :value}]
+  (utils.hex/pad (if value "0x1" "0x0")))
+
+(defmethod param :address
+  [{value :value}]
+  (utils.hex/pad (str/lower-case value)))
+
+(defmethod param :bytes
+  [{value :value}]
+  (utils.hex/pad value {:dir :right}))
+
+(defmethod param :dbytes
+  [{value :value}]
+  (let [size (utils.hex/size value)
+        padded (if (not= 0 (mod size 32))
+                 (utils.hex/pad value {:dir :right :size (* 32 (Math/ceil (/ size 32)))})
+                 value)]
+    (utils.hex/concat [(param {:type "uint" :value size}) padded])))
+
+(defmethod param :unumber
+  [{value :value}]
+  (utils.hex/pad (format "%x" (BigInteger. (str value)))))
+
+(defmethod param :number
+  [{value :value}]
+  (utils.hex/pad (format "%x" (if (< value 0)
+                                (.add (BigInteger. (str value)) (.shiftLeft (BigInteger. (str "1")) (* 32 8)))
+                                (BigInteger. (str value))))))
+
+(defmethod param :string
+  [{value :value}]
+  (let [hex-value (str "0x" (Hex/toHexString (.getBytes value StandardCharsets/UTF_8)))
+        size (utils.hex/size hex-value)
+        parts-length (Math/ceil (/ size 32))]
+    (->> (range 0 parts-length)
+         (map (fn [i] (utils.hex/pad (subs hex-value (* i 32) (min (count hex-value) (* (inc i) 32))) {:dir :right})))
+         (concat [(param {:type "uint" :value size})])
+         utils.hex/concat)))
+
+(defn signature [function-abi-item]
+  (-> (utils.hex/keccak-256 (utils.abi/function-item->signature function-abi-item))
+      (subs 0 8)))
+
+(defn function-data
+  [{abi-item :abi-item args :args}]
+  (utils.hex/concat [(signature abi-item)
+                     (param {:type "tuple" :components (:inputs abi-item) :value args})]))
