c2148d970aa47e848a186406c25d905153129864
fsys admin <admin@fsystems.io>
2025-03-19 03:21:04 -0300

    Improve keccak256
    - support hex string encoding
    - support utf8 string encoding
    - support bytes encoding

--- src/main/abi_clj/utils/hex.clj
+++ src/main/abi_clj/utils/hex.clj
@@ -6,15 +6,6 @@
    (org.bouncycastle.jcajce.provider.digest Keccak$Digest256)
    (org.bouncycastle.util.encoders Hex)))
 
-(defn keccak-256
-  [input]
-  (let [digest (Keccak$Digest256.)
-        input-bytes (if (string? input)
-                      (.getBytes input StandardCharsets/UTF_8)
-                      input)]
-    (.update digest input-bytes)
-    (Hex/toHexString (.digest digest))))
-
 (defn ->number
   [hex]
   (BigInteger. (str/replace hex #"0x" "") 16))
@@ -53,3 +44,40 @@
        (str/replace #"^0x" "")
        (clojure.core/subs start end)
        concat)))
+
+(defn- str->byte-arr [s]
+  (let [without0x (str/replace s #"^0x" "")
+        len (count without0x)
+        even (= 0 (rem len 2))
+        char-seq (seq without0x)
+        [head body] (if even
+                      [[] char-seq]
+                      [[(Character/digit (first char-seq) 16)] (rest char-seq)])]
+    (->>
+     body
+     (partition-all 2)
+     (map (fn [[a b]] (+ (bit-shift-left (Character/digit a 16) 4) (Character/digit b 16))))
+     (into head)
+     byte-array)))
+
+(defn- k-dispatch [s]
+  (cond (and (string? s) (re-matches #"^0x.*" s)) :hex-str
+        (string? s)            :str
+        :else                  :bytes))
+
+(defmulti keccak-256 k-dispatch)
+
+(defmethod keccak-256 :hex-str
+  [s]
+  (keccak-256 (str->byte-arr s)))
+
+(defmethod keccak-256 :str
+  [s]
+  (keccak-256 (.getBytes s StandardCharsets/UTF_8)))
+
+(defmethod keccak-256 :bytes
+  [input]
+  (let [digest (Keccak$Digest256.)
+        len (count input)]
+    (.update digest input 0 len)
+    (concat [(Hex/toHexString (.digest digest))])))
